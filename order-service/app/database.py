3dc81721b6beff1cca83adbc32d98f5b74619c49
Munam Mubashir <10995414-aightmunam@users.noreply.gitlab.com>
2023-10-06 00:23:21 +0500

    fix init models typo

--- order-service/app/database.py
+++ order-service/app/database.py
@@ -4,11 +4,11 @@ from tortoise.contrib.fastapi import register_tortoise
 
 
 def init_db(app: FastAPI) -> None:
-    Tortoise.init_models(['app.users.models'], 'models')
+    Tortoise.init_models(['app.orders.models'], 'models')
     register_tortoise(
         app,
         db_url='postgres://order:order@order-postgres:5432/order',
-        modules={'models': ['app.users.models']},
+        modules={'models': ['app.orders.models']},
         generate_schemas=True,
         add_exception_handlers=True,
     )
@@ -18,7 +18,7 @@ TORTOISE_ORM = {
     'connections': {'default': 'postgres://order:order@order-postgres:5432/order'},
     'apps': {
         'models': {
-            'models': ['app.users.models', 'aerich.models'],
+            'models': ['app.orders.models', 'aerich.models'],
             'default_connection': 'default',
         },
     },
